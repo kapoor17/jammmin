7334294f00769c008e62c7fa5fad32a71c0d90e5
Himanshu Kapoor <himanshu.kapur17@gmail.com>
2023-03-02 22:39:09 +0530

    static components added

---
+++ src/Components/Playlist/Playlist.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Playlist.css"
+
+const Playlist = () => {
+    return (
+        <div class="Playlist">
+            <input value="New Playlist"/>
+            {/* <!-- Add a TrackList component --> */}
+            <button class="Playlist-save">SAVE TO SPOTIFY</button>
+        </div>
+    );
+};
+
+export default Playlist;
